c0ca27b416aba6d24d137003a43c9ec818ee29c5
zaidsheikh7456-jpg <zaidsheikh7456@gmail.com>
2025-11-21 18:37:39 +0500

    Add files via upload

---
+++ zaid sheikh.py
@@ -0,0 +1,54 @@
+# print("python is fun")
+# name="zaid"
+# age=17
+# print(name,age)
+# print(1,2,3,sep="/#")
+# print("HELLO",end="+")
+# print("world")
+# print("learning","python","is","easy",sep="-",end="!/n")
+# print(10,20,sep=", ")
+# print("zaid", "17" ,"karachi" ,sep="  ")
+# print("welcome to python,let's learn together")
+# print(2000000000000000)
+# print(10e15)
+# print(1e308)
+# print(1.6e19)
+# print(True)
+# print(False)
+# a="abc"
+# type(a)
+# print(type(a))
+# num1=input("enter first no")
+# print(num1)
+# num2=input("enter second no")
+# print(num2))
+# num1=input("enter first no")
+# num2=input("enter second no")
+# num1=float(num1)
+# num2=float(num2)
+# print(num1+num2) 
+# a=10
+# b=a
+# print(b)
+# b+=a
+# print(b)
+# a=4
+# b="4"
+# b=int(b)
+# if a==b:
+#     print("A is equal to B")
+# else:
+#    print("A is not equal to B"
+# a=10
+# b=100
+# if a>b:
+#     print("A is greater than B")
+# else:
+#     print("B is greater than A")
+# name1=input("type name1 here")
+# name2=input("type name 2 here")
+
+# if name1==name2:
+#     print("names are same")
+# else:
+#     print("names are not same")
